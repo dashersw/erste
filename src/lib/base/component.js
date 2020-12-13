8f9fc39b1fc0fdf5f3a8536397eceb5a7786187f
Armagan Amcalar <armagan@amcalar.com>
2020-12-13 05:08:03 +0100

    Mark a component as rendered immediately after it's rendered

--- src/lib/base/component.js
+++ src/lib/base/component.js
@@ -298,6 +298,7 @@ export default class Component extends EventEmitter {
         }
 
         this.rendered_ = true;
+        ComponentManager.getInstance().markComponentRendered(this);
 
         this.onAfterRender();
         this.onAfterRenderHooks();
